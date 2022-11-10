2adb26cb9e58c9ba3194b372e8dba71d62438fc7
Aleksandra Wilk <alex.dev.wilk@gmail.com>
2022-11-10 12:33:50 +0100

    generate list of articles on side menu

---
+++ js/options.js
@@ -0,0 +1,8 @@
+const opt = {
+    links: '.titles a',
+    activeLinks: '.titles a.active',
+    activeArticles: '.posts article.active',
+    articleSelector: '.post',
+    titleSelector: '.post-title',
+    titleListSelector: '.titles'
+}

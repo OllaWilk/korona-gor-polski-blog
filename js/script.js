bd33fe6f221e543b4ca7d7f6b57749c98b613f26
OllaWilk <aleksandra.wilk.berserksoft@gmail.com>
2022-12-06 20:11:07 +0100

    add array of allTags to collect tags and generate them in tags menu

--- js/script.js
+++ js/script.js
@@ -65,6 +65,8 @@
   generateTitleLinks();
 
   function generateTags() {
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
     /* find all articles */
     const allArticles = document.querySelectorAll(opt.articleSelector);
 
@@ -87,12 +89,22 @@
         const linkHTML = `<li><a href="#tag-${tag}">${tag}</a></li>`;
         /* add generated code to html variable */
         html = html + linkHTML;
+        /* [NEW] check if this link is NOT already in allTags */
+        if (allTags.indexOf(linkHTML) == -1) {
+          /* [NEW] add generated code to allTags array */
+          allTags.push(linkHTML);
+        }
         /* END LOOP: for each tag */
       }
       /* insert HTML of all the links into the tags wrapper */
       tagList.innerHTML = html;
       /* END LOOP: for every article: */
     }
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector('.tags');
+
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
   }
   generateTags();
 
